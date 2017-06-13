0e87a81393abd2161ecc4d5dba4384328d077b0c
DocPrior <Sara Prior>
2017-06-13 18:32:43 -0400

    change quantity form integer to decimal

---
+++ db/migrate/20170613220559_change_quantity_to_decimal.rb
@@ -0,0 +1,5 @@
+class ChangeQuantityToDecimal < ActiveRecord::Migration[5.0]
+  def change
+  
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613212304) do
+ActiveRecord::Schema.define(version: 20170613220905) do
 
   create_table "locations", force: :cascade do |t|
     t.string "city"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170613212304) do
     t.string  "name"
     t.string  "manfacturer"
     t.decimal "cost"
-    t.integer "quantity"
+    t.decimal "quantity"
   end
 
   create_table "widgets", force: :cascade do |t|
